247f38f35cd67c98543361096978573eaa3d9d00
Rodrigo Gonzalez Hernandez <jordan23rodrigo@gmail.com>
2019-07-19 11:30:58 -0400

    navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+
+# Book.destroy_all
+
+# Book.create(
+#     title: 'libro 1',
+#     description: 'descripcion del libro y una critica',
+#     image: "https://picsum.photos/300/200/?=1")
+
+# Book.create(
+#     title: 'libro 2',
+#     description: 'descripcion del libro y una critia',
+#     image: "https://picsum.photos/300/200/?=2")
+
+
+# Book.create(
+#     title: 'libro 3',
+#     description: 'descripcion del libro y una criti',
+#     image: "https://picsum.photos/300/200/?=3")
+
+# Book.create(
+#     title: 'libro 3',
+#     description: 'descripcion del libro y una criti',
+#     image: "https://picsum.photos/300/200/?=4")
+
+# Book.create(
+#     title: 'libro 4',
+#     description: 'descripcion del libro y una criti',
+#     image: "https://picsum.photos/300/200/?=5")
+
+# Book.create(
+#     title: 'libro 5',
+#     description: 'descripcion del libro y una criti',
+#     image: "https://picsum.photos/300/200/?=6")
+
+# Book.create(
+#     title: 'libro 6',
+#     description: 'descripcion del libro y una criti',
+#     image: "https://picsum.photos/300/200/?=7")
+
+# Book.create(
+#     title: 'libro 7',
+#     description: 'descripcion del libro y una criti',
+#     image: "https://picsum.photos/300/200/?=8")
